883149c90c711fc3e23f6f5631bc1928f8a53230
Tia Shelley <calitastic@gmail.com>
2013-06-14 12:36:48 -0500

    removed online access needed for loginview

--- as_test/LoginViewController.m
+++ as_test/LoginViewController.m
@@ -33,34 +33,34 @@
 }
 
 - (IBAction)beginLogin {
-    //NSLog(@"%@, %@", _loginField.text, _passwordField.text);
-    NSString *myRequestString = [[NSString alloc] initWithFormat:@"userid=%@&password=%@", _loginField.text, _passwordField.text ];
-    NSData *myRequestData = [ NSData dataWithBytes: [ myRequestString UTF8String ] length: [ myRequestString length ] ];
-    NSMutableURLRequest *request = [ [ NSMutableURLRequest alloc ] initWithURL: [ NSURL URLWithString: @"http://10.0.1.100/~evl/as/validate2.php" ] ];
-    [ request setHTTPMethod: @"POST" ];
-    [ request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"content-type"];
-    [ request setHTTPBody: myRequestData ];
-    //NSLog(@"%@", request);
-    NSString *content;
-    while( !content){
-        NSURLResponse *response;
-        NSError *err;
-        NSData *returnData = [ NSURLConnection sendSynchronousRequest: request returningResponse:&response error:&err];
-        NSLog(@"error: %@", err);
-
-        if( [returnData bytes]) content = [NSString stringWithUTF8String:[returnData bytes]];
-        NSLog(@"responseData: %@", content);
-    }
-    if ([content isEqual: @"no such user"]  || [content hasPrefix:@"Access denied"]){
-        [self alertStatus:@"Login Failed. Please try again" :@"Login Failed"];
-        _passwordField.text = @"";
-    } else {
+    NSLog(@"%@, %@", _loginField.text, _passwordField.text);
+//    NSString *myRequestString = [[NSString alloc] initWithFormat:@"userid=%@&password=%@", _loginField.text, _passwordField.text ];
+//    NSData *myRequestData = [ NSData dataWithBytes: [ myRequestString UTF8String ] length: [ myRequestString length ] ];
+//    NSMutableURLRequest *request = [ [ NSMutableURLRequest alloc ] initWithURL: [ NSURL URLWithString: @"http://10.0.1.100/~evl/as/validate2.php" ] ];
+//    [ request setHTTPMethod: @"POST" ];
+//    [ request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"content-type"];
+//    [ request setHTTPBody: myRequestData ];
+//    //NSLog(@"%@", request);
+//    NSString *content;
+//    while( !content){
+//        NSURLResponse *response;
+//        NSError *err;
+//        NSData *returnData = [ NSURLConnection sendSynchronousRequest: request returningResponse:&response error:&err];
+//        NSLog(@"error: %@", err);
+//
+//        if( [returnData bytes]) content = [NSString stringWithUTF8String:[returnData bytes]];
+//        NSLog(@"responseData: %@", content);
+//    }
+//    if ([content isEqual: @"no such user"]  || [content hasPrefix:@"Access denied"]){
+//        [self alertStatus:@"Login Failed. Please try again" :@"Login Failed"];
+//        _passwordField.text = @"";
+//    } else {
         TabController *tc = [self.storyboard instantiateViewControllerWithIdentifier:@"TabController"];
-        NSRange range = [content rangeOfString:@"\n"];
         tc.user = _loginField.text;
-        tc.scientist = [content substringToIndex:range.location];
+    tc.scientist = _loginField.text;
         [self presentViewController:tc animated:YES completion:nil];
-    }
+    
+//    }
 }
 
 - (void) alertStatus:(NSString *)msg :(NSString *)title
